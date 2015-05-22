c90017caa7d39c130c8a821e5685ba10951b6b0e
LudoMC <ludovic@russo.fr>
2015-05-22 23:02:31 +0200

    First checkin

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# JH03-Project
+# Coursera - Getting and Cleaning Data - Course Project
+
+### Prerequisites
+The script run_analysis.R expect to have the "UCI HAR DataSet" folder in ./
+
+###Output
+When run, the script will output, in ./ a TIDY.TXT file which was uploaded as the answer to the first question
+
+###Explanations
+I decided to go for a wide tidy data frame.
+Each line will have all measurement means for a single subject/activity pair.
+
+There will then be 180 observations (30 subjects x 6 activities)
+
+###High-level view of the script
+
+* Read the Activities IDs and their corresponding names (WALKING, LAYING, ...)
+* Read the Features IDs and their corresponding names (all the 561 measures names)
+    + For the training set
+    + Get all activities IDs for all the observations
+    + Get all the Features values for all the observations
+    + Get all the subjects IDs for all the observations
+    + build a data frame with Subject ID, Activity name, all measures
+* For the testing set
+    + Same as for training set
+* Merge training and test sets into a OneSet
+* Build a vector which will contain all the columns to keep
+    + Put all columns having mean()
+    + Add all columns having std()
+    + Shift them as they now start at index 3
+    + Add first two columns (Subject and Activity)
+* Create a subset of OneSet cleverly names OneSubSet which contains all the data we need and not more
+* The data frame which will be the final tidy one is creating empty, on the model of the OneSubSet
+* Two simple nested for loops will fill it
+    + Loop on all subjects
+        +Loop on all activities
+            + Add to the tidy data frame a new line with the Subject, Activity and the means of all measurement columns
+* Use write.table() to output the tidy data frame
---
+++ run_analysis.R
@@ -0,0 +1,71 @@
+## Get Labels for each activity ID
+ActivityLabels <- read.table(file="./UCI HAR Dataset/activity_labels.txt",
+                             sep=" ",
+                             col.names = c("Id","Activity"),
+                             stringsAsFactors = FALSE)
+## Get Labels for each features ID
+FeaturesLabels <- read.table(file="./UCI HAR Dataset/features.txt",
+                             sep=" ",
+                             col.names = c("Id", "Feature"))
+
+##Training set
+#IDs of Activities
+TrainLabels <- read.table(file="./UCI HAR Dataset/train/y_train.txt",
+                          sep=" ",
+                          col.names = c("Activity Id"))
+#Main data with column names being features name
+TrainSet <- read.table(file="./UCI HAR Dataset/train/X_train.txt",
+                       col.names = FeaturesLabels$Feature)
+#IDs of Subjects
+TrainSubjects <- read.table(file="./UCI HAR Dataset/train/subject_train.txt",
+                            col.names = c("Subject Id"))
+#Constructing main Train DF
+Train <- data.frame(Subject=TrainSubjects$Subject.Id) # Subject ID
+Train <- cbind(Train, Activity=as.character(ActivityLabels$Activity[TrainLabels$Activity.Id])) # Activity name
+Train <- cbind(Train, TrainSet) # Measures from wearable gadget
+
+##Test set
+#IDs of Activities
+TestLabels <- read.table(file="./UCI HAR Dataset/test/y_test.txt",
+                         sep=" ",
+                         col.names = c("Activity Id"))
+#Main data with column names being features name
+TestSet <- read.table(file="./UCI HAR Dataset/test/X_test.txt",
+                      col.names = FeaturesLabels$Feature)
+#IDs of Subjects
+TestSubjects <- read.table(file="./UCI HAR Dataset/test/subject_test.txt",
+                           col.names = c("Subject Id"))
+#Constructing main Test DF
+Test <- data.frame(Subject=TestSubjects$Subject.Id) # Subject ID
+Test <- cbind(Test, Activity=as.character(ActivityLabels$Activity[TestLabels$Activity.Id])) # Activity name
+Test <- cbind(Test, TestSet) # Measures from wearable gadget
+
+
+##Merge Train & Test sets
+OneSet <- rbind(Train,Test)
+
+#Keeping only std() and mean() columns + Activity and Subject.Id (last two columns)
+colsToKeep <- as.vector(FeaturesLabels$Id[grepl("mean()",FeaturesLabels$Feature)])
+colsToKeep <- c(colsToKeep, as.vector(FeaturesLabels$Id[grepl("std()",FeaturesLabels$Feature)]))
+colsToKeep <- colsToKeep + 2
+colsToKeep <- c(1, 2, colsToKeep)
+library(dplyr)
+#Subsetting the global set keeping only the columns selected above
+OneSubSet <- select(OneSet, colsToKeep)
+OneSubSet$Activity <- as.character(OneSubSet$Activity)
+
+#Construct the (wide) tidy data frame
+#One line per subject per activity and 89 columns of the mean of each measure
+tidyDF <- subset(OneSubSet, 1==0)
+idx <- 0
+for(s in min(OneSubSet$Subject):max(OneSubSet$Subject)){ # For each subject
+    tmp <- filter(OneSubSet, Subject==s)
+    for (a in 1:nrow(ActivityLabels)) { # For each activity for this subject
+        tmp.act <- filter(tmp, Activity == ActivityLabels$Activity[[a]])
+        idx <- idx + 1
+        tidyDF[idx,] <- c(tmp.act$Subject[1], tmp.act$Activity[1], colMeans(tmp.act[3:81]))
+    }
+}
+
+#Creating the file to be uploaded for the Course project
+write.table(tidyDF, file = "./UCI HAR Dataset/TIDY.TXT", sep = " ", quote = FALSE, row.name=FALSE)
